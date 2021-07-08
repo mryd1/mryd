533f577a5080de147b971cb90a6538d5f0aeb370
HYM740 <71169815+HYM740@users.noreply.github.com>
2021-07-08 15:48:16 +0800

    Create util.c

---
+++ util.c
@@ -0,0 +1,12 @@
+double sum(double *arrays,int length)
+{
+  //自己实现
+}
+double average(double *arrays,int length)
+{
+  //自己实现
+}
+double total(double score1,double score2,double per1,double per2)
+{
+  //自己实现
+}
