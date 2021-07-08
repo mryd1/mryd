e78cd97585c7e27ec9cad755354a517c350863be
HYM740 <71169815+HYM740@users.noreply.github.com>
2021-07-08 16:18:31 +0800

    Update and rename main.c to src/main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
 #define LEN sizeof(struct stu)
 
 struct stu
